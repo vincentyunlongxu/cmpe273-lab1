d57848ad797a9867b9592c67f89fcde715e81e50
vincentyunlongxu <yunlong.xu@sjsu.edu>
2015-09-16 00:09:26 -0700

    Update sleep.go

--- sleep.go
+++ sleep.go
@@ -1,7 +1,23 @@
-package sleep
+package main
 
-import "time"
+import ("time"
+		"fmt")
 
-func sleep(time int){
-	<-time.After(time.Second * time.Duration(time))
+func main() {
+	c := make(chan string, 1)
+	fmt.Println("1")
+	go func () {
+		sleep(4)
+		fmt.Println("2")
+		c <- "2"
+	}()
+	fmt.Println("3")
+	select{
+	case result := <- c:
+		fmt.Println(result)
+	}
+}
+
+func sleep(num int) {
+	<-time.After(time.Second * time.Duration(num))
 }
